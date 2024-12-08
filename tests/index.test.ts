349acf20c76d685b5ff708ff439b18a6623d1c0b
Denis Tagaev <sivean.27@gmail.com>
2024-12-07 19:36:04 -0500

    Basic functionality tests including setup and db calls mockup

---
+++ tests/index.test.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { app, server } from "../src"; 
+import { connectDB } from "../src/config/db";
+
+jest.mock("../src/config/db", () => ({
+  connectDB: jest.fn(),
+}));
+
+describe("Server Initialization", () => {
+  beforeAll(async () => {
+    // Mocking successful database connection
+    (connectDB as jest.Mock).mockImplementation(() => Promise.resolve());
+  });
+
+  afterAll(async () => {
+    // Ensuring the database connection is closed after tests
+    await mongoose.connection.close();
+    server.close();
+  });
+
+  //ensuring connection with the database is established
+  it("should connect to the database", () => {
+    expect(connectDB).toHaveBeenCalled();
+  });
+
+  //ensuring the api has started to listen for requests
+  it("should listen on the specified port", () => {
+    const PORT = process.env.PORT || 3000;
+    expect(app.listen).toBeDefined();
+    console.log(`Server expected to run on port ${PORT}`);
+  });
+});
