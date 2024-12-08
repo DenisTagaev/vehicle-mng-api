ac1e1a7f76ee685b1a23cfaaad59cf19cab14e19
Denis Tagaev <sivean.27@gmail.com>
2024-12-08 03:01:34 -0500

    Added cross-environment support config

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import express, { Express } from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import vehicleRoutes from "./routes/index";
+import path from 'path';
 
-dotenv.config();
+const env:string = process.env.NODE_ENV ?? "development";
+const envPath:string = path.resolve(
+  process.cwd(),
+  `.env${env === "development" ? "" : "." + env}`
+);
+
+dotenv.config({ path: envPath });
 const app:Express = express();
 
 const PORT:string | 3000 = process.env.PORT ?? 3000;
@@ -16,5 +23,7 @@ connectDB();
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+console.log('NODE_ENV', process.env.NODE_ENV);
 
 export { app, server };
